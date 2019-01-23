951f813084bcce0f24c2ab6834b2af98859d06d4
Hassan Nasouri <moshkou2000@gmail.com>
2019-01-24 01:14:34 +0800

    contacts indicator 002

--- app/src/main/java/com/moshkou/md/Activities/ContactsActivity.java
+++ app/src/main/java/com/moshkou/md/Activities/ContactsActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.text.Editable;
@@ -12,15 +13,19 @@ import android.text.TextWatcher;
 import android.text.method.KeyListener;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.moshkou.md.Adapters.ContactsAdapter;
+import com.moshkou.md.BuildConfig;
 import com.moshkou.md.Configs.Flags;
 import com.moshkou.md.Configs.Keys;
 import com.moshkou.md.Configs.Permission;
@@ -38,13 +43,17 @@ public class ContactsActivity extends Activity {
     private Activity activity = this;
 
     private LinearLayout permissionView;
+    private RelativeLayout grantedView;
     private TextView allow;
-    private FrameLayout header;
     private EditText search;
     private ListView listView;
+    private ListView indicators;
+    private TextView indicator;
 
     private List<ContactModel> contacts;
     private ContactsAdapter adapter;
+
+    private boolean flagAllow = false;
 
 
     @Override
@@ -53,32 +62,58 @@ public class ContactsActivity extends Activity {
         setContentView(R.layout.activity_contacts);
 
         permissionView = findViewById(R.id.permissionView);
+        grantedView = findViewById(R.id.grantedView);
         allow = findViewById(R.id.allow);
-        header = findViewById(R.id.header);
         search = findViewById(R.id.search);
         listView = findViewById(R.id.contacts);
+        indicators = findViewById(R.id.indicators);
+        indicator = findViewById(R.id.indicator);
+
+        indicators.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                return (event.getAction() == MotionEvent.ACTION_MOVE);
+            }
+        });
 
         allow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                flagAllow = true;
+
                 Permission.Check.READ_CONTACTS(activity);
+                if (Permission.READ_CONTACTS)
+                    setAdapter();
             }
         });
 
         search.addTextChangedListener(new TextWatcher() {
 
             @Override
-            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) { }
+            public void beforeTextChanged(CharSequence cs, int start, int count, int after) { }
 
             @Override
             public void afterTextChanged(Editable arg0) {}
 
             @Override
-            public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
-                if (arg3 == 0)
+            public void onTextChanged(CharSequence cs, int start, int before, int count) {
+                if (start + before + count == 0)
                     adapter.restore();
                 else
                     adapter.filter(cs);
+            }
+        });
+
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if (contacts != null && contacts.size() > 0) {
+                    indicator.setText("" + contacts.get(firstVisibleItem).name.charAt(0));
+                }
             }
         });
 
@@ -97,17 +132,19 @@ public class ContactsActivity extends Activity {
                 if (permissions.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     setAdapter();
                 } else if (shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS)) {
-                    header.setVisibility(View.GONE);
-                    listView.setVisibility(View.GONE);
+                    grantedView.setVisibility(View.GONE);
                     permissionView.setVisibility(View.VISIBLE);
+                } else if (flagAllow) {
+                    startActivity(new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + BuildConfig.APPLICATION_ID)));
                 }
                 break;
         }
+
+        flagAllow = false;
     }
 
     private void setAdapter() {
-        header.setVisibility(View.VISIBLE);
-        listView.setVisibility(View.VISIBLE);
+        grantedView.setVisibility(View.VISIBLE);
         permissionView.setVisibility(View.GONE);
 
         contacts = ContactsControl.getContacts(activity);
