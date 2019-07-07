8281514216df427aa7b5d39d02e3453a922a8960
Hassan Nasouri <moshkou2000@users.noreply.github.com>
2019-07-07 23:16:32 +0800

    fcm

---
+++ app/src/main/java/com/moshkou/md/services/FCMWorker.java
@@ -0,0 +1,27 @@
+package com.moshkou.md.services;
+
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+
+public class FCMWorker extends Worker {
+
+    private static final String TAG = "Volley";
+
+    public FCMWorker(@NonNull Context appContext, @NonNull WorkerParameters workerParams) {
+        super(appContext, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        Log.d(TAG, "Performing long running task in scheduled job");
+        // TODO(developer): add long running task here.
+        return Result.success();
+    }
+}
